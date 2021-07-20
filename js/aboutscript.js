03c78a7c2379492ce32e75ae04ab438f4be12ba0
shaniajorense <shania.orense@yahoo.com>
2021-07-20 15:15:45 +0200

    almost done

--- js/aboutscript.js
+++ js/aboutscript.js
@@ -3,37 +3,39 @@
 var teachers = [{
     name:"Christoph Pirringer",
     status: "Founder & CEO",
-    img: "./img/Christoph.png"
+    img: "../img/img-teach/Christoph.png"
 },{
     name:"Lisa Duschek",
     status: "Founder & CEO",
-    img: "./img/Lisa.png"
+    img: "../img/img-teach/Lisa.png"
 },{
     name: "Giath Serri",
     status:"Trainer",
-    img:"./img/Serri.png"
+    img:"../img/img-teach/Serri.png"
 },{
     name:"Irati Larreina",
     status:"Trainer",
-    img:"./img/Irati.png"
+    img:"../img/img-teach/Irati.png"
 },{
     name:"Acilio Carraro",
     status:"Trainer",
-    img:"./img/Acilio.png"
+    img:"../img/img-teach/Acilio.png"
 },{
     name:"Smilja Simikic",
     status:"Secretary",
-    img:"./img/Smilija.jpg"
+    img:"../img/img-teach/Smilija.jpg"
 }]
 
 for (let val of teachers){
-    document.getElementsByClassName("teachers")[0].innerHTML +=`
-    <div class="col col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center">
+    document.getElementsByClassName("teachers")[0].innerHTML +=`<div class="col col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center">
+    <div class="container p-5">
+    
     <div class="card" style="width: 18rem;">
   <img src="${val.img} " class="card-img-top" alt="teachers">
   <div class="card-body">
     <p class="card-text d-flex justify-content-center">${val.name} </p>
     <p class="card-text text-muted d-flex justify-content-center">${val.status} </p>
   </div>
+</div>
 </div>`
 }
